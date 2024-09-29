80ca8ae431b6cceaab4742c56f3ecdf1165cbab1
Nimmo-san <nat25oloman@gmail.com>
2024-09-29 11:40:11 +0100

    init weather api using fastapi

---
+++ weather_api.py
@@ -0,0 +1,14 @@
+from fastapi import FastAPI
+from typing import List
+import sqlite3
+import pandas as pd
+
+
+app = FastAPI()
+
+def get_weather_data_from_db():
+    """
+    
+    """
+    
+    pass
